e2a4ad598f8c5e3380e7b15a0686c0a864d8a1a0
pratham meena <yashmeena274@gmail.com>
2024-03-05 02:04:14 +0530

    updating sockt.io file

--- socketapi.js
+++ socketapi.js
@@ -1,45 +1,47 @@
 const io = require("socket.io")();
-const userModel = require('./routes/users')
-const messageModel = require('./routes/message')
-const groupModel = require('./routes/group')
+const userModel = require('./routes/users.js');
+const messageModel = require('./routes/message.js');
+const groupModel = require('./routes/group.js');
+
 const socketapi = {
     io: io
 };
 
 // Add your socket.io logic here!
-io.on("connection", function (socket) {
+io.on("connection", async function (socket) {
     console.log("A user connected");
 
-    // 1-1 chat 
-    socket.on('join-user', async (username) => {
-        var loggedInUser = await userModel.findOneAndUpdate({ username }, { socketId: socket.id })
-    })
+    try {
+        // Handle 1-1 chat
+        socket.on('join-user', async (username) => {
+            await userModel.findOneAndUpdate({ username }, { socketId: socket.id });
+        });
 
-    socket.on('sendByuser', async (messageObject) => {
-        var oppositeUser = await userModel.findOneAndUpdate({ username: messageObject.reciever }, { socketId: socket.id })
-        socket.to(oppositeUser.socketId).emit('sendByServer', messageObject);
-        var newMessage = await messageModel.create({
-            ...messageObject,
-        })
-    })
+        socket.on('sendByuser', async (messageObject) => {
+            const oppositeUser = await userModel.findOneAndUpdate({ username: messageObject.receiver }, { socketId: socket.id });
+            socket.to(oppositeUser.socketId).emit('sendByServer', messageObject);
+            await messageModel.create({ ...messageObject });
+        });
 
+        // Handle group chat
+        socket.on('joinGroup', async (groupName) => {
+            socket.join(groupName);
+            console.log(socket.id, "joined", groupName);
+        });
 
-    // groupChat
-    socket.on('joinGroup', async groupName => {
-        socket.join(groupName);
-        console.log(socket.id, "joined", groupName)
-    });
-
-    socket.on('sendMessageInGroup', async messageObject => {
-        socket.to(messageObject.groupName).emit('receiveMessageInGroup', messageObject);
-        var newMessage = await messageModel.create({
-            sender:messageObject.sender,
-            reciever: messageObject.groupName,
-            message:messageObject.message,
-        })
-    })
-
+        socket.on('sendMessageInGroup', async (messageObject) => {
+            socket.to(messageObject.groupName).emit('receiveMessageInGroup', messageObject);
+            await messageModel.create({
+                sender: messageObject.sender,
+                receiver: messageObject.groupName,
+                message: messageObject.message,
+            });
+        });
+    } catch (error) {
+        console.error("Socket.IO Error:", error);
+    }
 });
-// end of socket.io logic
+
+// End of Socket.IO logic
 
 module.exports = socketapi;
